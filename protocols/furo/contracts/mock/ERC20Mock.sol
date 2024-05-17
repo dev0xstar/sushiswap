7724a4735c6db5dab5b1e66e7590017bdcddea5f
Kakao-max <angrybird4837@gmail.com>
2024-05-17 01:30:43 -0400

    updated

--- protocols/furo/contracts/mock/ERC20Mock.sol
+++ protocols/furo/contracts/mock/ERC20Mock.sol
@@ -8,7 +8,13 @@ contract ERC20Mock is ERC20 {
     event Deposit(address indexed dst, uint256 wad);
     event Withdrawal(address indexed src, uint256 wad);
 
-    
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 supply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, supply);
+    }
 
     
 

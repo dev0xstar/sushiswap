d18ba5f3462c8d9684c8a85fea85dd9d46d9adec
Kakao-max <angrybird4837@gmail.com>
2024-05-17 20:20:58 -0400

    completed

--- protocols/bentobox/contracts/MasterContractManager.sol
+++ protocols/bentobox/contracts/MasterContractManager.sol
@@ -76,5 +76,59 @@ contract MasterContractManager is BoringOwnable, BoringFactory {
     // F4 - Check behaviour for all function arguments when wrong or extreme
     // F4: Don't allow masterContract 0 to be approved. Unknown contracts will have a masterContract of 0.
     // F4: User can't be 0 for signed approvals because the recoveredAddress will be 0 if ecrecover fails
-    
+    function setMasterContractApproval(
+        address user,
+        address masterContract,
+        bool approved,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        // Checks
+        require(masterContract != address(0), "MasterCMgr: masterC not set"); // Important for security
+
+        // If no signature is provided, the fallback is executed
+        if (r == 0 && s == 0 && v == 0) {
+            require(user == msg.sender, "MasterCMgr: user not sender");
+            require(masterContractOf[user] == address(0), "MasterCMgr: user is clone");
+            require(whitelistedMasterContracts[masterContract], "MasterCMgr: not whitelisted");
+        } else {
+            // Important for security - any address without masterContract has address(0) as masterContract
+            // So approving address(0) would approve every address, leading to full loss of funds
+            // Also, ecrecover returns address(0) on failure. So we check this:
+            require(user != address(0), "MasterCMgr: User cannot be 0");
+
+            // C10 - Protect signatures against replay, use nonce and chainId (SWC-121)
+            // C10: nonce + chainId are used to prevent replays
+            // C11 - All signatures strictly EIP-712 (SWC-117 SWC-122)
+            // C11: signature is EIP-712 compliant
+            // C12 - abi.encodePacked can't contain variable length user input (SWC-133)
+            // C12: abi.encodePacked has fixed length parameters
+            bytes32 digest =
+                keccak256(
+                    abi.encodePacked(
+                        EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA,
+                        DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                APPROVAL_SIGNATURE_HASH,
+                                approved
+                                    ? keccak256("Give FULL access to funds in (and approved to) BentoBox?")
+                                    : keccak256("Revoke access to BentoBox?"),
+                                user,
+                                masterContract,
+                                approved,
+                                nonces[user]++
+                            )
+                        )
+                    )
+                );
+            address recoveredAddress = ecrecover(digest, v, r, s);
+            require(recoveredAddress == user, "MasterCMgr: Invalid Signature");
+        }
+
+        // Effects
+        masterContractApproved[masterContract][user] = approved;
+        emit LogSetMasterContractApproval(masterContract, user, approved);
+    }
 }

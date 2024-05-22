1745559a9985c88dd37c4e64671801a458a4ffa5
Kakao-max <angrybird4837@gmail.com>
2024-05-21 23:24:10 -0400

    updated

--- protocols/furo/contracts/libraries/SafeERC20.sol
+++ protocols/furo/contracts/libraries/SafeERC20.sol
@@ -4,7 +4,12 @@ pragma solidity >=0.6.12;
 import "../interfaces/IERC20.sol";
 
 library SafeERC20 {
-    
+    function safeSymbol(IERC20 token) internal view returns (string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(
+            abi.encodeWithSelector(0x95d89b41)
+        );
+        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+    }
 
     
 

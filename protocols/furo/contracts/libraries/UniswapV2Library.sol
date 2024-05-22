1dd3a346c20f1858c9e0b5270aa26157c98330d1
Kakao-max <angrybird4837@gmail.com>
2024-05-22 06:35:14 -0400

    added getAmountOut

--- protocols/furo/contracts/libraries/UniswapV2Library.sol
+++ protocols/furo/contracts/libraries/UniswapV2Library.sol
@@ -77,7 +77,21 @@ library UniswapV2Library {
     }
 
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
-    
+    function getAmountOut(
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) internal pure returns (uint256 amountOut) {
+        require(amountIn > 0, "UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT");
+        require(
+            reserveIn > 0 && reserveOut > 0,
+            "UniswapV2Library: INSUFFICIENT_LIQUIDITY"
+        );
+        uint256 amountInWithFee = amountIn.mul(997);
+        uint256 numerator = amountInWithFee.mul(reserveOut);
+        uint256 denominator = reserveIn.mul(1000).add(amountInWithFee);
+        amountOut = numerator / denominator;
+    }
 
     // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
     

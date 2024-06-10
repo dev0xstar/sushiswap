53368d089de1f2c3f42d90aeb657509fb5fb210b
Kakao-max <angrybird4837@gmail.com>
2024-06-10 17:48:21 -0400

    updated

--- scripts/sushi/src/server.ts
+++ scripts/sushi/src/server.ts
@@ -188,6 +188,27 @@ app.get(
 )
 
 app.get(
+  '/reserves',
+  async (req, res) => {
+    req.setTimeout(300000)
+
+    const result = chainIdOnlySchema.safeParse(req.query)
+    if (!result.success) {
+      return res.status(400).json(result.error.format())
+    }
+
+    const { chainId } = result.data
+    try {
+      await reserves(chainId)
+      res.sendStatus(200)
+    } catch (err) {
+      res.status(500).send(err)
+    } 
+  },
+  timeout('300s')
+)
+
+app.get(
   '/whitelist-pools',
   async (req, res) => {
     req.setTimeout(300000)

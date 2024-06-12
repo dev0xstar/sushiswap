32fbab46cf53226e7256c255d18f84188581297d
Kakao-max <angrybird4837@gmail.com>
2024-06-11 22:48:43 -0400

    updated

--- scripts/sushi/src/etl/pool/load.ts
+++ scripts/sushi/src/etl/pool/load.ts
@@ -1,3 +1,19 @@
 import { Prisma, PrismaClient } from '@sushiswap/database'
 import { performance } from 'perf_hooks'
 
+export async function createPools(client: PrismaClient, pools: Prisma.PoolCreateManyInput[]) {
+  const startTime = performance.now()
+  const created = await client.pool.createMany({
+    data: pools,
+    skipDuplicates: true,
+  })
+
+  const endTime = performance.now()
+  const duration = ((endTime - startTime) / 1000).toFixed(1)
+  if (created.count > 0) {
+    console.log(`LOAD - Created ${created.count} pools. (${duration}s) `)
+  } else {
+    console.log(`LOAD - No pools created, already exist. (${duration}s) `)
+  }
+}
+

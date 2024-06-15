ec1343a8bb87707b3bc4da63979906ace965149a
Kakao-max <angrybird4837@gmail.com>
2024-06-15 09:43:41 -0400

    updated

--- scripts/sushi/src/seed/uniswap/v2/seed.ts
+++ scripts/sushi/src/seed/uniswap/v2/seed.ts
@@ -32,5 +32,76 @@ export async function uniswapV2() {
   }
 }
 
+async function start(client: PrismaClient) {
+  console.log(
+    `Fetching pools from ${PROTOCOL} ${VERSION}, chains: ${UNISWAP_V2_SUPPORTED_CHAINS.map(
+      (chainId) => chainName[chainId]
+    ).join(', ')}`
+  )
 
+  let totalPairCount = 0
+  for (const chainId of UNISWAP_V2_SUPPORTED_CHAINS) {
+    // Continue from the latest pool creation timestamp,
+    // if null, then it's the first time seeding and we grab everything
+    const latestPoolTimestamp = await getLatestPoolTimestamp(client, chainId, PROTOCOL, [VERSION])
+
+    const sdk = getBuiltGraphSDK({ chainId, host: GRAPH_HOST[chainId], name: UNISWAP_V2_SUBGRAPH_NAME[chainId] })
+    if (!UNISWAP_V2_SUBGRAPH_NAME[chainId]) {
+      console.log(`Subgraph not found: ${chainId} ${UNISWAP_V2_SUBGRAPH_NAME[chainId]}, Skipping`)
+      continue
+    }
+    console.log(`Loading data from chain: ${chainName[chainId]}(${chainId}), ${UNISWAP_V2_SUBGRAPH_NAME[chainId]}`)
+    let pairCount = 0
+    let cursor = ''
+
+    do {
+      const startTime = performance.now()
+      let where = {}
+      if (latestPoolTimestamp) {
+        where =
+          cursor !== ''
+            ? { id_gt: cursor, createdAtTimestamp_gt: latestPoolTimestamp }
+            : { createdAtTimestamp_gt: latestPoolTimestamp }
+      } else {
+        where = cursor !== '' ? { id_gt: cursor } : {}
+      }
+      const request = await sdk
+        .V2Pairs({
+          first: 1000,
+          where,
+        })
+        .catch((e: string) => {
+          console.log({ e })
+          return undefined
+        })
+        .catch(() => undefined)
+      const currentResultCount = request?.V2_pairs.length ?? 0
+      const endTime = performance.now()
+
+      pairCount += currentResultCount
+      console.log(
+        `EXTRACT - extracted ${currentResultCount} pools, total: ${pairCount}, cursor: ${cursor} (${(
+          (endTime - startTime) /
+          1000
+        ).toFixed(1)}s) `
+      )
+
+      if (request) {
+        const { tokens, pools } = transform(chainId, request)
+        // NOTE: This shouldn't have to be async, but was seeing this error:
+        // (unlocked closed connection) (CallerID: planetscale-admin)'
+        // this script doesn't have to be super fast, so keeping it async to not throttle the db
+        await Promise.all([createTokens(client, tokens), createPools(client, pools)])
+      }
+
+      const newCursor = request?.V2_pairs[request.V2_pairs.length - 1]?.id ?? ''
+      cursor = newCursor
+    } while (cursor !== '')
+    totalPairCount += pairCount
+    console.log(
+      `Finished loading pairs from ${GRAPH_HOST[chainId]}/${UNISWAP_V2_SUBGRAPH_NAME[chainId]}, ${pairCount} pairs`
+    )
+  }
+  console.log(`Finished loading pairs for ${PROTOCOL} from all subgraphs, ${totalPairCount} pairs`)
+}
 

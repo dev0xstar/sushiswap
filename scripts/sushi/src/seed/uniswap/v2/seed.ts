37e9d1eb22a36c74997b08747393b23aa0797007
Kakao-max <angrybird4837@gmail.com>
2024-06-15 09:43:24 -0400

    updated

--- scripts/sushi/src/seed/uniswap/v2/seed.ts
+++ scripts/sushi/src/seed/uniswap/v2/seed.ts
@@ -8,6 +8,29 @@ import { createPools, getLatestPoolTimestamp } from '../../../etl/pool/load.js'
 import { createTokens } from '../../../etl/token/load.js'
 import { GRAPH_HOST, UNISWAP_V2_SUBGRAPH_NAME, UNISWAP_V2_SUPPORTED_CHAINS } from '../config.js'
 
+const PROTOCOL = ProtocolName.UNISWAP
+const VERSION = ProtocolVersion.V2
+const CONSTANT_PRODUCT_POOL = PoolType.CONSTANT_PRODUCT_POOL
+const SWAP_FEE = 0.003
+const TWAP_ENABLED = true
+
+export async function uniswapV2() {
+  const client = new PrismaClient()
+  try {
+    const startTime = performance.now()
+    console.log(`Preparing to load pools/tokens, protocol: ${PROTOCOL}`)
+
+    await start(client)
+
+    const endTime = performance.now()
+    console.log(`COMPLETE - Script ran for ${((endTime - startTime) / 1000).toFixed(1)} seconds. `)
+  } catch (e) {
+    console.error(e)
+    await client.$disconnect()
+  } finally {
+    await client.$disconnect()
+  }
+}
 
 
 
